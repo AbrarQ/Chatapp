a1fa2d4e98f783ff61a8ccb3b411c25c24ccb8da
AbrarQ <104406765+AbrarQ@users.noreply.github.com>
2023-03-06 08:27:40 +0530

    OPTIMISING CHAT FRONT END

--- chathome/chathome.js
+++ chathome/chathome.js
@@ -15,7 +15,7 @@ try{
 
     await axios.post("http://localhost:1000/chat/postchat",chatObj, {headers :{ "Authorization": localStorage.getItem("token") }})
     .then(response =>{
-       console.log(response)  
+  
         document.getElementById("chatinput").value = "" ;
         
     })   
@@ -35,40 +35,66 @@ try{
 
 window.addEventListener("DOMContentLoaded", async () => {
 
+    localStorage.removeItem("recentChats")
     const response =await axios.get("http://localhost:1000/chat/getchat", {headers :{ "Authorization": localStorage.getItem("token") }})
     .then(response => {return (response.data.chat)})   
+   
     
     let validator = response.length
     
-    if(response != null){
-        printMessages(response,0)
-    }
+    const ls = localStorage.getItem("recentChats");
+    
+    if(ls != null){
+        printMessages(ls)
+  }else if( ls == null){
+    savetoLS(validator,response)
+    printMessages(response)
+
+  }
 
 
     
 
-//    setInterval(async ()=>{
-//     console.log("printing at each second")
-// const checker = await axios.get("http://localhost:1000/chat/getchat", {headers :{ "Authorization": localStorage.getItem("token") }})
-// .then(response => {return (response.data.chat)})   
-//     console.log("checker length",checker.length)
-//     console.log("validator length",validator)
-// if(checker.length > validator){
-//   printMessages(checker,validator)
-//   validator = checker.length;
-// }},1000)
+
+    
+
+   setInterval(async ()=>{
+    console.log("printing at each second")
+const checker = await axios.get("http://localhost:1000/chat/getchat", {headers :{ "Authorization": localStorage.getItem("token") }})
+.then(response => {return (response.data.chat)})   
+   
+if(checker.length > validator){
+
+  printMessages(checker.slice(validator))
+  validator = checker.length;
+  savetoLS(checker.length,checker)
+}
+},1000)
 
 
 
 })
 
 
- function printMessages(response,x){
-    console.log("printing")
-    for( let i =x; i< response.length;i++){
+ function printMessages(response){
+    
+    for( let i =0; i< response.length;i++){
         const parent = document.getElementById("messageprinter")
         parent.innerHTML+= ` <div class="message">
                                  <p class="text-secondary">${response[i].userLogin.name} : ${response[i].chat}</p>
                              </div>`
     }
+}
+
+
+function savetoLS(chatLength, chatArr){
+
+    const recentChatLength = chatLength-10;
+    
+    const recentChat = chatArr.slice(recentChatLength)
+
+    console.log(JSON.stringify(recentChat))
+   
+  localStorage.setItem("recentChats",JSON.stringify(recentChat) )
+    
 }
